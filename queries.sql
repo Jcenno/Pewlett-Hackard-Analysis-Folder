ed0b3f1137f1598321443f699cef26142b882743
Joseph Enno <jceno@gmail.com>
2021-07-02 15:32:32 -0500

    submitting challenge

--- queries.sql
+++ queries.sql
@@ -169,3 +169,20 @@ title
 INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no, to_date DESC;
+
+-- write a query to create Mentorship Eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
