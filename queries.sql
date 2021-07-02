f61fbcc9c2319557c5cde838e14af462d7a76f11
Joseph Enno <jceno@gmail.com>
2021-07-02 14:21:13 -0500

    challenge upload

--- queries.sql
+++ queries.sql
@@ -86,7 +86,8 @@ SELECT * FROM current_emp;
 
 
 -- Employee count by department number
-SELECT COUNT(ce.emp_no), de.dept_no
+SELECT COUNT(ce.emp_no), 
+de.dept_no
 INTO count_dep
 FROM current_emp as ce
 LEFT JOIN dept_emp as de
@@ -143,3 +144,28 @@ INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
 
 SELECT * FROM dept_info;
+
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+--GROUP BY e.emp_no, ti.emp_no
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
